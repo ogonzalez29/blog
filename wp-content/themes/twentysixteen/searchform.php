56f91c2bb8ee86609af01eff0bcbd77af551c487
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-04-22 17:45:02 -0500

    fixed search form button icon format

--- wp-content/themes/twentysixteen/searchform.php
+++ wp-content/themes/twentysixteen/searchform.php
@@ -13,5 +13,5 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentysixteen' ); ?></span>
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentysixteen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'twentysixteen' ); ?>" />
 	</label>
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentysixteen' ); ?></span></button>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( '', 'submit button', 'twentysixteen' ); ?></span></button>
 </form>
